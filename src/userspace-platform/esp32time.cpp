34d7bb2d18f597661c724b0b0a36d5cbb56a02a6
Tom Morton <tomm@riseup.net>
2023-11-11 20:24:41 +0000

    Implement ESP32Time in USERSPACE mode

--- src/userspace-platform/esp32time.cpp
+++ src/userspace-platform/esp32time.cpp
@@ -1,13 +1,20 @@
 #include "ESP32Time.h"
+#include <ctime>
 
 ESP32Time::ESP32Time(int) {}
 
-int ESP32Time::getYear() { return 2020; }
-int ESP32Time::getMonth() { return 0; }
-int ESP32Time::getDay() { return 0; }
-int ESP32Time::getDayofWeek() { return 0; }
-int ESP32Time::getDayofYear() { return 0; }
-int ESP32Time::getHour(bool) { return 0; }
-int ESP32Time::getMinute() { return 0; }
-int ESP32Time::getSecond() { return 0; }
+// nasty function returns pointer to static space...
+std::tm *const now() {
+	std::time_t t = std::time(nullptr);
+	return std::localtime(&t);
+}
+
+int ESP32Time::getYear() { return now()->tm_year; }
+int ESP32Time::getMonth() { return now()->tm_mon; }
+int ESP32Time::getDay() { return now()->tm_mday; }
+int ESP32Time::getDayofWeek() { return now()->tm_wday; }
+int ESP32Time::getDayofYear() { return now()->tm_yday; }
+int ESP32Time::getHour(bool) { return now()->tm_hour; }
+int ESP32Time::getMinute() { return now()->tm_min; }
+int ESP32Time::getSecond() { return now()->tm_sec; }
 void ESP32Time::setTime(int, int, int, int, int, int) {}
