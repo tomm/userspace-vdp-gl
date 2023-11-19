19a0e8f3c8a0b502c6c59f9b3e1f7bba5e70acd7
jackokring <jackokring@gmail.com>
2023-11-19 02:32:09 +0000

    no progress on seg fault arm64

--- src/userspace-platform/esp32time.cpp
+++ src/userspace-platform/esp32time.cpp
@@ -3,11 +3,14 @@
 
 ESP32Time::ESP32Time(int) {}
 
+static std::time_t ttime;
+
 // nasty function returns pointer to static space...
 std::tm *const now() {
-	std::time_t t = std::time(nullptr);
-	return std::localtime(&t);
+	ttime = std::time(nullptr);
+	return std::localtime(&ttime);
 }
+
 
 int ESP32Time::getYear() { return now()->tm_year; }
 int ESP32Time::getMonth() { return now()->tm_mon; }
@@ -18,3 +21,4 @@ int ESP32Time::getHour(bool) { return now()->tm_hour; }
 int ESP32Time::getMinute() { return now()->tm_min; }
 int ESP32Time::getSecond() { return now()->tm_sec; }
 void ESP32Time::setTime(int, int, int, int, int, int) {}
+
